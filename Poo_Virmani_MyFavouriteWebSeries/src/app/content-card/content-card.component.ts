5dfae12065ed854b3d34fc5c93fcc7effc721792
Poorva001 <122814199+Poorva001@users.noreply.github.com>
2024-01-22 17:31:10 -0500

    Update content-card.component.ts

--- Poo_Virmani_MyFavouriteWebSeries/src/app/content-card/content-card.component.ts
+++ Poo_Virmani_MyFavouriteWebSeries/src/app/content-card/content-card.component.ts
@@ -17,6 +17,7 @@ export class ContentCardComponent {
       title: "Everybody Loves Raymond",
       description:"A comedy series about the everyday life of sports columnist Ray Barone and his dysfunctional family.",
       creator:"Philip Rosenthal",
+      imgURL: "https://tse4.mm.bing.net/th?id=OIP.GLVKnfpwJF6x9T-7roI34QHaFj&pid=Api&P=0&h=180",
       type:"Comedy"
     });
     this.contentList.addContent({
@@ -24,6 +25,7 @@ export class ContentCardComponent {
       title: "How I met your Mother",
       description:" A comedy series about Ted and how he fell in love.",
       creator:"Craig Thomas and Carter Bays",
+      imgURL: "https://tse1.mm.bing.net/th?id=OIP.cWB0IEW0Y71dRAvydC7JdAHaEK&pid=Api&P=0&h=180",
       type:"Drama"
     });
     this.contentList.addContent({
@@ -31,6 +33,7 @@ export class ContentCardComponent {
       title: "Permanent Roommates",
       description:"A TVF series about a couple who face the prospect of getting married after a long distance relationship and a surprise visit from the groom's roommate.",
       creator:"Arunabh Kumar",
+      imgURL: "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/c6891ba199e71ce3c613b23cd6daaf8dbdd6485c0dc1cbbcc77e5f1d7c25ae4e._V_SX450_.png",
       type:"Romance"
     });
   }
