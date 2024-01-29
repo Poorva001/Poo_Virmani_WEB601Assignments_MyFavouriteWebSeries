eed0961a68daadeadabf364f471e1dc7688aef16
Poorva001 <122814199+Poorva001@users.noreply.github.com>
2024-01-28 21:11:12 -0500

    the content is displaying on the main page

--- Poo_Virmani_MyFavouriteWebSeries/src/app/app.component.ts
+++ Poo_Virmani_MyFavouriteWebSeries/src/app/app.component.ts
@@ -1,14 +1,71 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ContentCardComponent } from './content-card/content-card.component';
+import { ContentListComponent } from './content-list/content-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContentCardComponent],
+  imports: [RouterOutlet, ContentListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Poo_Virmani_MyFavouriteWebSeries';
+  Items =[
+    {
+      id: 1,
+      title: "Everybody Loves Raymond",
+      description:"A comedy series about the everyday life of sports columnist Ray Barone and his dysfunctional family.",
+      creator:"Philip Rosenthal",
+      imgURL: "https://image.tmdb.org/t/p/original/vkXeCdMqaZlCoWCoKdDYgs4Ynrg.jpg",
+      type:"Comedy",
+      tags: ["emotions", "relationships", "laughter"]
+    },
+    {
+      id: 2,
+      title: "How I met your Mother",
+      description:" A comedy series about Ted and how he fell in love.",
+      creator:"Craig Thomas and Carter Bays",
+      imgURL: "https://picfiles.alphacoders.com/400/thumb-1920-400793.jpg",
+      type:"Drama",
+      tags: ["emotions", "humor", "laughter"]
+    },
+    {
+      id: 3,
+      title: "Permanent Roommates",
+      description:"A TVF series about a couple who face the prospect of getting married after a long distance relationship and a surprise visit from the groom's roommate.",
+      creator:"Arunabh Kumar",
+      imgURL: "https://m.media-amazon.com/images/M/MV5BNzZlNTlmNTEtNDVmNi00M2I5LWE0MDMtNTQ5ZTgwOTA2NzA0XkEyXkFqcGdeQXVyMzc5NDk0NzQ@._V1_FMjpg_UX1000_.jpg",
+      type:"Romance",
+      tags: ["emotions", "relationships"]
+    },
+    {
+      id: 4,
+      title: "Vikings",
+      description: "Follow the legendary Viking chieftain Ragnar Lothbrok and his descendants as they raid and explore new territories.",
+      creator: "Michael Hirst",
+      imgURL: "https://movieposters2.com/images/1512284-b.jpg",
+      type: "Historical Drama",
+      tags: ["Vikings", "war", "exploration"]
+  },
+  {
+      id: 5,
+      title: "Luna Nera",
+      description: "A young witch discovers her true powers in a world where witchcraft is both a source of power and a danger to its practitioners.",
+      creator: "Tiziana Triana",
+      imgURL: "https://fr.web.img3.acsta.net/r_1280_720/pictures/20/04/20/06/11/3746828.jpg",
+      type: "Fantasy",
+      tags: ["witchcraft", "magic", "coming-of-age"]
+  },
+  {
+      id: 6,
+      title: "Cursed",
+      description: "A reimagining of the Arthurian legend, following Nimue, a young heroine with a mysterious gift, as she seeks to save her people.",
+      creator: "Tom Wheeler, Frank Miller",
+      imgURL: "https://www.themoviedb.org/t/p/original/bLDi1pdZnhXkv4XayCocCIuHrn4.jpg",
+      type: "Fantasy Adventure",
+      tags: ["Arthurian legend", "magic", "heroine"]
+  }
+    
+  ];
 }
--- Poo_Virmani_MyFavouriteWebSeries/src/app/content-card/content-card.component.ts
+++ Poo_Virmani_MyFavouriteWebSeries/src/app/content-card/content-card.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ContentList } from '../helper-files/content-list';
 
 @Component({
   selector: 'app-content-card',
@@ -9,32 +8,4 @@ import { ContentList } from '../helper-files/content-list';
   styleUrl: './content-card.component.scss'
 })
 export class ContentCardComponent {
-  contentList: ContentList = new ContentList();
-
-  constructor(){
-    this.contentList.addContent({
-      id: 0,
-      title: "Everybody Loves Raymond",
-      description:"A comedy series about the everyday life of sports columnist Ray Barone and his dysfunctional family.",
-      creator:"Philip Rosenthal",
-      imgURL: "https://tse4.mm.bing.net/th?id=OIP.GLVKnfpwJF6x9T-7roI34QHaFj&pid=Api&P=0&h=180",
-      type:"Comedy"
-    });
-    this.contentList.addContent({
-      id: 1,
-      title: "How I met your Mother",
-      description:" A comedy series about Ted and how he fell in love.",
-      creator:"Craig Thomas and Carter Bays",
-      imgURL: "https://tse1.mm.bing.net/th?id=OIP.cWB0IEW0Y71dRAvydC7JdAHaEK&pid=Api&P=0&h=180",
-      type:"Drama"
-    });
-    this.contentList.addContent({
-      id: 2,
-      title: "Permanent Roommates",
-      description:"A TVF series about a couple who face the prospect of getting married after a long distance relationship and a surprise visit from the groom's roommate.",
-      creator:"Arunabh Kumar",
-      imgURL: "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/c6891ba199e71ce3c613b23cd6daaf8dbdd6485c0dc1cbbcc77e5f1d7c25ae4e._V_SX450_.png",
-      type:"Romance"
-    });
   }
-}
